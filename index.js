d9156b70cd9c5e41ed0f8a101984519f666a056c
Padde <patrick_kloes@arcor.de>
2013-09-30 23:12:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Reader = require('./lib/Reader.js'),
+    GeoIP2;
+
+GeoIP2 = module.export = function GeoIP2(database) {
+    if (database === undefined) {
+        throw new Error('GeoIP2: No Database given');
+    }
+
+    this.reader = new Reader(database);
+};
+
+GeoIP2.prototype.getGeoData = function getGeoData(ipAddress) {
+    this.reader.get(ipAddress);
+};
+
+GeoIP2.prototype.getDatabaseMetadata = function getDatabaseMetadata() {
+    return this.reader.getMetadata();
+};
